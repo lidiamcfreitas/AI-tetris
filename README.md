76a75874b8d22bc0ace90e755faad7df4836d56b
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-10-22 18:53:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Artificial Intelligence Tetris Game
+
+On this project we intended to implement a search algorithm able to play a variant of the game Tetris, in which the parts are known for putting on departure.
+
+Our goal is to try to maximize the number points to the defined parts.
+
+To simplify the game, is only allowed the player to choose the rotation and the position where the piece will fall.
+
+Once chosen the rotation and position, the piece should fall right without any further movement.
+
+## Details about Lisp Enviroment
+
+For changing line:
+
+```
+> (format t "~%")
+
+NIL
+```
+
+> See description of the [Format](http://www.lispworks.com/documentation/lw50/CLHS/Body/f_format.htm) in [HyperSpec](http://www.lispworks.com/documentation/HyperSpec/Front/).
+
+To load a source file for the Lisp environment:
+
+```
+(load <nome-ficheiro>)
+```
+
+For example:
+
+```
+> (load "projecto.lisp")
+
+T
+```
+
+To compile a file:
+
+```
+> (compile-file "projecto")
+
+#P"/Users/pedromatos/Desktop/projecto/projecto.fasl"
+
+NIL
+
+NIL
+```
+
+> Compilation should not identify warnings!
+
+To load the last well compiled file:
+```
+> (load "projecto")
+
+T
+```
---
+++ tetris.lisp
@@ -0,0 +1,135 @@
+; GRUPO 29
+;
+;
+; Bruno Cardoso
+; 72619
+;
+; Francisco Maria Calisto
+; 70916
+;
+; Lidia Carvalho de Freitas
+; 78559
+;
+
+
+; (load "examples.fas")
+; (load "my-test.lisp")
+
+; ======================================================================================= ;
+;                               DEFINICAO DAS ESTRUTURAS DE DADOS                         ;
+; ======================================================================================= ;
+
+
+(defstruct estado
+    (pontos 0)
+    (num-vars 0)
+    (restricoes NIL)
+    (var-doms NIL)
+    (var-restrs NIL)
+    (empty-vars NIL)
+    (dirty NIL)
+    (atribuicoes NIL))
+
+
+; ======================================================================================= ;
+; ======================================================================================= ;
+;                                                                                         ;
+;                                  PROJECT | FIRST PART                                   ;
+;                                                                                         ;
+; ======================================================================================= ;
+; ======================================================================================= ;
+
+
+; ======================================================================================= ;
+;                                        TIPO RESTRICAO                                   ;
+; ======================================================================================= ;
+
+(defun accao (coluna peca)
+    (cons coluna peca))
+
+(defun cria-accao (int array_pecas)
+    (accao int array_pecas))
+    
+(defun accao-coluna (acc)
+    (car acc))
+    
+(defun accao-peca (acc)
+    (cdr acc))
+
+(defun cria-tabuleiro ()
+    (let ((lines 18) (columns 10))
+	(defparameter board (make-array (list lines columns)))
+	(dotimes (i lines)
+	    (dotimes (j columns)
+		(setf (aref board i j) NIL)))
+    board))
+
+(defun copia-tabuleiro(tabuleiro-arg)
+    (let ((linhas 18) (colunas 10))
+        (defparameter tabuleiro-novo (make-array (list linhas colunas)))
+        (dotimes (i linhas)
+            (dotimes (j colunas)
+                (setf (aref tabuleiro-novo i j) (aref tabuleiro-arg i j))))
+        tabuleiro-novo))
+	
+(defun tabuleiro-preenchido-p (tabuleiro l c) 
+    (aref tabuleiro l c))
+
+(defun tabuleiro-altura-coluna(tabuleiro c)
+    (let ((altura 0))
+        (dotimes(l 18)
+	    (if (aref tabuleiro l c) 
+	        (progn  (setf altura (- 18 l)) 
+	        (setf l 18) ) () ))
+	altura))
+
+(defun tabuleiro-linha-completa-p(tabuleiro l)
+    (let ((p T))
+    (dotimes(columns 10)
+        (if (aref tabuleiro l columns) () (setf p nil )))
+    p))
+
+(defun tabuleiro-preenche! (tabuleiro l c)
+    (if (and (<= l 17) (<= c 9)) 
+        (setf (aref tabuleiro (- 17 l) c) T)))
+
+(defun tabuleiro-despreenche (tabuleiro l c)
+    (setf (aref tabuleiro (- 17 l) c) NIL))
+
+(defun tabuleiro-remove-linha!(tabuleiro l)
+    (dotimes(c 10)
+        (tabuleiro-despreenche tabuleiro l c)))
+
+(defun tabuleiro-topo-preenchido(tabuleiro)
+    (let ((p NIL) (l 17))
+        (dotimes(columns 10)
+            (if (aref tabuleiro l columns) 
+                (progn  (setf p T ) 
+                        (setf columns 10)) 
+                ()))
+    p)) 
+
+(defun tabuleiros-iguais-p(tabuleiro1 tabuleiro2)
+    (equalp tabuleiro1 tabuleiro2))
+
+(defun tabuleiro->array(tabuleiro)  ; rever com professor
+        (copia-tabuleiro tabuleiro))
+
+(defun array->tabuleiro(array)      ; rever com professor
+    (let ((tabuleiro NIL))
+        (setf tabuleiro (copia-tabuleiro array))
+        tabuleiro))
+
+
+(load "utils.lisp")
+
+
+;TESTING
+(let ((tabuleiro NIL) (tabuleiro-novo NIL))
+    (setf tabuleiro (cria-tabuleiro))
+    (tabuleiro-preenche! tabuleiro 0 0)
+    (setf tabuleiro-novo (copia-tabuleiro tabuleiro ))
+    ;(tabuleiro-despreenche tabuleiro-novo 0 0)
+    (tabuleiro-preenche! tabuleiro-novo 2 0)
+    (tabuleiro-preenche! tabuleiro 2 0)
+)
