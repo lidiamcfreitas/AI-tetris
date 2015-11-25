54306aecff3962a70868f1273a50cb20fe6c3b6b
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-11-25 20:32:37 +0000

    [UPDATE]

--- heap.lisp
+++ heap.lisp
@@ -10,32 +10,32 @@
 
 (defun heap-length (heap) (length heap))
 
-(defun heapify (heap-list i n)
+(defun heapify (heap-list i)
     (let (  (left       (left i))
             (right      (right i))
             (largest    -1)
-            (aux        nil))
+            (aux        nil)
+            (len        (heap-length heap-list)))
 
-            (if (>= left (nth i heap-list)) 
+            (if (and (< left len) (>= (nth left heap-list) (nth i heap-list))) 
                 (setf largest left)
                 (setf largest i))
 
-            (if (>= right (nth i heap-list))
+            (if (and (< right len) (>= (nth right heap-list) (nth i heap-list)))
                 (setf largest right))
 
             (if (not (eq largest i))
                 ; swap
                 (progn
                     (setf aux (nth i heap-list))
-                    (setf (nth i heap-list) (nth largest heap-list))
+                    (setf (nth i heap-list) (nth largest heap-list)=)
                     (setf (nth largest heap-list) aux)
-                    (heapify heap-list largest n)))))
+                    (heapify heap-list largest)))))
 
-;(defun build-heap (heap-list)
-;    (let (  (len (heap-length heap-list) ) 
-;            (upvalue (/ (heap-length heap-list) 2)))
-;        (loop for i from upvalue downto 0 do (
-;            (heapify heap-list i len)))))
+(defun build-heap (heap-list)
+    (let ((upvalue (floor (heap-length heap-list) 2)))
+        (loop for i from upvalue downto 0 do 
+            (heapify heap-list i))))
 
 
 
