098c277e90e09288c6c9ca25cbb9ad850b24f2d5
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-08 22:43:12 +0000

    needed

--- lispscript.lisp
+++ lispscript.lisp
@@ -6,14 +6,21 @@
 (setf valor1 (with-input-from-string (in (nth 1 *args*))(read in)))
 (setf valor2 (with-input-from-string (in (nth 2 *args*))(read in)))
 (setf valor3 (with-input-from-string (in (nth 3 *args*))(read in)))
+(setf valor4 )
 
-(load "f.lisp")
+(load "entregue.lisp")
 ;(load "utils.lisp")
 
-;(ignore-value (setf a1 '#2A((T T T T NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL))))
+(ignore-value (setf a1 '#2A((T T T T NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(T T T NIL NIL NIL T T T T)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL)(NIL NIL NIL NIL NIL NIL NIL NIL NIL NIL))))
 ;;;deve retornar IGNORE
 ;(setf r1 (procura-best a1 '(t i l l)))
 ;(procura-best a1 '(t i l l))
 
+;(dotimes (c 20)
+;	(print (cria-tabuleiro-aleatorio 1 0.085)))
 
-(procura-best (cria-tabuleiro-aleatorio 1 0.185) (random-pecas 6))
+;(procura-best (cria-tabuleiro) (random-pecas 6))
+
+;(time (procura-best (cria-tabuleiro-aleatorio 1 0.085) (random-pecas 8)))
+
+(procura-best (cria-tabuleiro-aleatorio 1 0.085) (random-pecas 6))
