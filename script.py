98d5c3b087676bd48103e8ee692f2cdc636d257f
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-08 00:40:48 +0000

    printing result

--- script.py
+++ script.py
@@ -9,7 +9,9 @@ heur_random_values = [random.random() for i in range(4)]
 
 print("random values:" , heur_random_values)
 
-batcmd="./lispscript.lisp {} {} {} {}".format(heur_random_values[0], heur_random_values[1], heur_random_values[2], heur_random_values[3])
-print("running command...    ", batcmd)
-result = subprocess.check_output(batcmd, shell=True)
+cmd="./lispscript.lisp {} {} {} {}".format(heur_random_values[0], heur_random_values[1], heur_random_values[2], heur_random_values[3])
+print("running command...    ", cmd)
+cmd_result = subprocess.check_output(cmd, shell=True)
+
+result = [int(s) for s in cmd_result.split() if s.isdigit()][0]
 print  (result)
