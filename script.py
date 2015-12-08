76b20e0e7e5324b9a581f289075401b06e8cd1c2
Lídia Freitas <lidiamcfreitas@gmail.com>
2015-12-08 01:21:10 +0000

    one per line

--- script.py
+++ script.py
@@ -9,9 +9,25 @@ heur_random_values = [random.random() for i in range(4)]
 
 print("random values:" , heur_random_values)
 
-cmd="./lispscript.lisp {} {} {} {}".format(heur_random_values[0], heur_random_values[1], heur_random_values[2], heur_random_values[3])
-print("running command...    ", cmd)
-cmd_result = subprocess.check_output(cmd, shell=True)
 
-result = [int(s) for s in cmd_result.split() if s.isdigit()][0]
+cmd="./lispscript.lisp {} {} {} {}".format(heur_random_values[0], heur_random_values[1], heur_random_values[2], heur_random_values[3])
+cmd_result = subprocess.check_output(cmd, shell=True)
+print("running command...    ", cmd)
+print("result: ", cmd_result)
+
+result = [int(s) for s in cmd_result.split() if s.isdigit()]
 print  (result)
+
+
+import re
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print ("matchObj.group() : ", matchObj.group())
+   print ("matchObj.group(1) : ", matchObj.group(1))
+   print ("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print ("No match!!")
